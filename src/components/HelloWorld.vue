56c68e58d315872e736a5148d6a6a0292d7f0d05
MhFahmi48 <m.husnolfahmi@student.uir.ac.id>
2023-05-19 11:27:13 +0700

    update routing

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,49 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="Black">{{ msg }}</h1>
+    <h3>
+      About Me <br>
+      <a href="https://www.instagram.com/mhfa4898/" target="_blank" rel="noopener"> <img src="@/assets/ig.png" alt=""> </a> &
+      <a href="https://vuejs.org/" target="_blank" rel="noopener"><img src="@/assets/github.png" alt=""></a>.
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+img {
+  width: 5%;
+  
+}
+
+
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <div >
+    <h1></h1>
+      <h2></h2>
+      <div>
+      <ul>
+        
+      </ul>
+      <div>
+
+      </div>
+    </div>
+  </div>
+  <footer>
+    <p>Copyright &copy;MHusnolFahmi</p>
+  </footer>
+</template>
+
