770678c3dd340eae3a5435a1980ef8312b7e1eb4
MhFahmi48 <m.husnolfahmi@student.uir.ac.id>
2023-05-19 11:47:20 +0700

    update link github

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -13,7 +13,7 @@ defineProps({
     <h3>
       About Me <br>
       <a href="https://www.instagram.com/mhfa4898/" target="_blank" rel="noopener"> <img src="@/assets/ig.png" alt=""> </a> &
-      <a href="https://vuejs.org/" target="_blank" rel="noopener"><img src="@/assets/github.png" alt=""></a>.
+      <a href="https://github.com/MhFahmi48/TugasFahmi" target="_blank" rel="noopener"><img src="@/assets/github.png" alt=""></a>.
     </h3>
   </div>
 </template>
